90eba92f834601c9c5f80e31e54da0b60b31b096
Kofey0K <Kofey0K@users.noreply.github.com>
2020-02-12 15:38:38 +0200

    Sharp lab1

---
+++ Lab1_sharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lab1_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static bool IsSmaller(int value1, int value2)
+        {
+            int size = Marshal.SizeOf(value2) * 8;
+            if ((value2 & 1 << size - 1) != 0 || (value1 & 1 << size - 1) != 0)
+                for (int i = 0; i < size; i++)
+                {
+                    if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
+                        return true;
+                    if ((value1 & 1 << i) != 0 && (value2 & 1 << i) == 0)
+                        return false;
+                }
+            else
+                for (int i = size - 1; i >= 0; i--)
+                {
+                    if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
+                        return true;
+                    if ((value1 & 1 << i) != 0 && (value2 & 1 << i) == 0)
+                        return false;
+                }
+            return false;
+        }
+    }
+}
--- prep1.cpp
+++ prep1.cpp
@@ -1,9 +1,28 @@
-﻿
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+void func1(int&);
 
-int main()
-{
+int main(){
+    int value = 63;
+    func1(value);
+
+    unsigned int s;
+
+    std::cout << "Enter the number of s" << endl;
+    std::cin >> s;
+    for (unsigned int mask = 1 << sizeof(int) * 8 - 1; mask; mask >>= 1)
+        std::cout << (s & mask ? '1' : '0');
+
     
 }
 
+void func1(int& value)
+{
+    for (int counter = 1; ((value ^= counter) & counter) == 0; counter <<= 1);
+}
+
+
+/*bool func2(int value, int op) {
+    for (int i = 1
+}*/
