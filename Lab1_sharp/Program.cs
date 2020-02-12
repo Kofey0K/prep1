d9bdd09b120da973f6097edc58a590958bc589e1
Kofey0K <Kofey0K@users.noreply.github.com>
2020-02-12 23:48:06 +0200

    dodelano

--- Lab1_sharp/Program.cs
+++ Lab1_sharp/Program.cs
@@ -7,28 +7,47 @@ namespace Lab1_sharp
     {
         static void Main(string[] args)
         {
-            
+            int a = -128;
+            PlusOne(ref a);
+            bool result = IsSmaller(33, 1);
         }
         static bool IsSmaller(int value1, int value2)
         {
-            int size = Marshal.SizeOf(value2) * 8;
-            if ((value2 & 1 << size - 1) != 0 || (value1 & 1 << size - 1) != 0)
+            bool result = true;
+            int size = Marshal.SizeOf(value1) * 8;
+            if ((value1 & 1 << size - 1) != 0 || (value2 & 1 << size - 1) != 0)
                 for (int i = 0; i < size; i++)
                 {
-                    if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
-                        return true;
                     if ((value1 & 1 << i) != 0 && (value2 & 1 << i) == 0)
-                        return false;
+                        result =  false;
+                    if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
+                        result = true;
                 }
             else
                 for (int i = size - 1; i >= 0; i--)
                 {
-                    if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
-                        return true;
                     if ((value1 & 1 << i) != 0 && (value2 & 1 << i) == 0)
-                        return false;
+                        result = false;
+                    if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
+                        result =  true;
                 }
-            return false;
+            if (value1 * value2 < 0)
+            {
+                if (result == true)
+                {
+                    result = false;
+                }
+                else
+                {
+                    result = true;
+                }
+                
+            }
+            return result;
+        }
+        static void PlusOne(ref int a)
+        {
+            for (int i = 1; ((a ^= i) & i) == 0; i <<= 1) ;
         }
     }
 }
--- prep1.cpp
+++ prep1.cpp
@@ -2,18 +2,13 @@
 using namespace std;
 
 void func1(int&);
+bool IsSmaller(int, int);
 
 int main(){
     int value = 63;
     func1(value);
 
-    unsigned int s;
-
-    std::cout << "Enter the number of s" << endl;
-    std::cin >> s;
-    for (unsigned int mask = 1 << sizeof(int) * 8 - 1; mask; mask >>= 1)
-        std::cout << (s & mask ? '1' : '0');
-
+    bool result =  IsSmaller(33, 2);
     
 }
 
@@ -21,8 +16,39 @@ void func1(int& value)
 {
     for (int counter = 1; ((value ^= counter) & counter) == 0; counter <<= 1);
 }
+bool IsSmaller(int value1, int value2)
+{
+    bool result = true;
+    int size = sizeof(value1) * 8;
+    if ((value1 & 1 << size - 1) != 0 || (value2 & 1 << size - 1) != 0)
+        for (int i = 0; i < size; i++)
+        {
+            if ((value1 & 1 << i) != 0 && (value2 & 1 << i) == 0)
+                result = false;
+            if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
+                result = true;
+        }
+    else
+        for (int i = size - 1; i >= 0; i--)
+        {
+            if ((value1 & 1 << i) != 0 && (value2 & 1 << i) == 0)
+                result = false;
+            if ((value2 & 1 << i) != 0 && (value1 & 1 << i) == 0)
+                result = true;
+        }
+    if (value1 * value2 < 0)
+    {
+        if (result == true)
+        {
+            result = false;
+        }
+        else
+        {
+            result = true;
+        }
+
+    }
+    return result;
+}
 
 
-/*bool func2(int value, int op) {
-    for (int i = 1
-}*/
